96345f7ff2fc3bc0f184ef2256272d4aae0aeeb6
assilbekov <askhat.assilbekov@gmail.com>
2024-04-26 16:18:17 -0300

    finish the implementation

--- pkg/data/json-data.go
+++ pkg/data/json-data.go
@@ -1,7 +1,17 @@
 package data
 
+import "fmt"
+
 type JSONDocument struct{}
 
-func (doc JSONDocument) ConvertToXML() string {
-	return "<xml>converted from JSON</xml>"
+func (doc JSONDocument) ConvertToXML(data string) string {
+	return fmt.Sprintf("<xml>%s</xml>", data)
+}
+
+type JSONDocumentAdapter struct {
+	JSONDocument *JSONDocument
+}
+
+func (adapter JSONDocumentAdapter) SendXMLData(data string) interface{} {
+	return adapter.JSONDocument.ConvertToXML(data)
 }
